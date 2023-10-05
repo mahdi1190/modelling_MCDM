67ceee37c1430a3563ba7721cc03d740ed678c7a
Mahdi Ahmed <smahmed3@sheffield.ac.uk>
2023-10-05 15:13:14 +0100

    asdasd

--- CCS/CCS_plot.py
+++ CCS/CCS_plot.py
@@ -10,14 +10,86 @@ import dash_html_components as html
 from dash import dcc, html, Input, Output, State
 from dash.dependencies import Input, Output
 
-
-df = pd.read_csv(r"C:\Users\Sheikh M Ahmed\modelling_MCDM\CCS\CCSdata.csv")
+last_mod_time = 0
+df = pd.read_csv(r"C:\Users\Sheikh M Ahmed\modelling_MCDM\CCS\CCS_data.csv")
 markets = pd.read_excel(r"C:\Users\Sheikh M Ahmed\modelling_MCDM\data\markets.xlsx")
 
-carbon_market = markets["carbon"].to_numpy()
+carbon_market = markets["carbon"].to_numpy()/1000
+
+max_co2_emissions = 18000
 
 co2_data = df['co2'].to_numpy()
 production_data = df['co2'].to_numpy()
+
+
+co2_flow = max(co2_data)
+
+app = dash.Dash(__name__)
+
+# Initial layout
+app.layout = html.Div([    
+    # Text elements at the top
+    html.Div([
+        html.Div(id='credits', style={'fontSize': 24}),
+    ], style={'display': 'flex', 'justifyContent': 'center'}),
+    
+    # Carbon Credits Graph
+    dcc.Graph(id='carbon-credits'),
+    
+    # Interval component
+    dcc.Interval(
+        id='interval-component',
+        interval=10 * 10000,  # Refresh every 10 seconds
+        n_intervals=0
+    )
+])
+
+# Callback for updating the carbon credits graph
+@app.callback(
+    Output('carbon-credits', 'figure'),
+    Output('credits', 'children'),
+    [Input('interval-component', 'n_intervals')]
+)
+def update_graphs(n_intervals):
+    global last_mod_time  # Declare as global to modify it
+
+    # Check last modification time of the model file
+    current_mod_time = os.path.getmtime(r"C:\Users\Sheikh M Ahmed\modelling_MCDM\CCS\CCS_plot.py")
+
+    if current_mod_time > last_mod_time:
+        last_mod_time = current_mod_time  # Update last modification time
+
+        model = pyomomodel()
+
+        carbon_offset = [model.credits_used_to_offset[h]() for h in model.INTERVALS]
+        carbon_sell = [model.credits_sold[h]() for h in model.INTERVALS]
+        credits = [model.carbon_credits[h]() for h in model.INTERVALS]
+        carbon_held = [model.credits_held[h]() for h in model.INTERVALS]
+        carbon_earn = [model.credits_earned[h]() for h in model.INTERVALS]
+
+        credits_layout = {
+            'template': 'plotly_dark',
+            'font': {'family': "Courier New, monospace", 'size': 12, 'color': "RebeccaPurple", 'weight': 'bold'},
+            'xaxis': {'title': 'Time Intervals'},
+            'yaxis': {'title': 'No of Credits'},
+            'title': 'Carbon Credit Dynamics',
+            'title_font': {'size': 24, 'family': 'Arial, sans-serif', 'weight': 'bold'},
+        }
+
+        carbon_credits_figure = {
+            'data': [
+                {'x': list(model.INTERVALS), 'y': credits, 'type': 'line', 'name': 'Carbon Credits Purchased'},
+                {'x': list(model.INTERVALS), 'y': carbon_sell, 'type': 'line', 'name': 'Carbon Credits Sold'},
+                {'x': list(model.INTERVALS), 'y': carbon_held, 'type': 'line', 'name': 'Carbon Credits Held'},
+                {'x': list(model.INTERVALS), 'y': carbon_earn, 'type': 'line', 'name': 'Carbon Credits Earned'},
+                {'x': list(model.INTERVALS), 'y': carbon_offset, 'type': 'line', 'name': 'Carbon Credits Used To Offset'},
+            ],
+            'layout': credits_layout,
+        }
+
+        return carbon_credits_figure, f"Credit Cost: NaN"
+    else:
+        raise dash.exceptions.PreventUpdate
 
 def pyomomodel():
     # Create model
@@ -29,7 +101,7 @@ def pyomomodel():
     model.TIME_PERIODS = Set(initialize=TIME_PERIODS)
 
 
-    no_intervals = 3
+    no_intervals = 30
     intervals_time = int(total_time / no_intervals)
     INTERVALS = list(range(no_intervals))  # Four 6-hour intervals in a day
     model.INTERVALS = Set(initialize=INTERVALS)
@@ -37,10 +109,20 @@ def pyomomodel():
 
     # -------------- Decision Variables --------------
     model.ccs_investment_decision = Var(model.TIME_PERIODS, within=Binary)
-    model.ccs_size = Var(within=NonNegativeReals)
+    model.CCS_capacity = Var(within=NonNegativeReals)
     model.carbon_captured = Var(model.TIME_PERIODS, within=NonNegativeReals)
     model.ccs_availability = Var(model.TIME_PERIODS, within=Binary)
+    model.total_emissions_per_interval = Var(model.INTERVALS, within=NonNegativeReals)
+    model.co2_emissions = Var(model.TIME_PERIODS, within=NonNegativeReals)
     # Additional decision variables as needed.
+    model.carbon_credits = Var(model.INTERVALS, within=NonNegativeReals)
+    model.credits_purchased = Var(model.INTERVALS, within=NonNegativeReals)
+    model.credits_earned = Var(model.INTERVALS, within=NonNegativeReals)
+    model.credits_sold = Var(model.INTERVALS, within=NonNegativeReals)
+    model.credits_held = Var(model.INTERVALS, within=NonNegativeReals)
+    model.credits_unheld = Var(model.INTERVALS, within=NonNegativeReals)
+    model.emissions_difference = Var(model.INTERVALS, within=NonNegativeReals)
+    model.credits_used_to_offset = Var(model.INTERVALS, within=NonNegativeReals)
 
     # -------------- Constraints --------------
     # Define constraints related to CCS size, carbon capture, investment decision, etc.
@@ -52,26 +134,50 @@ def pyomomodel():
             return model.ccs_availability[t] == model.ccs_availability[t-1] + model.ccs_investment_decision[t]
     model.system_purchased_con = Constraint(model.TIME_PERIODS, rule=system_purchased)
 
+    def co2_emissions_rule(model, t):
+        return model.co2_emissions[t] == (co2_data[t])
+    model.co2_emissions_constraint = Constraint(model.TIME_PERIODS, rule=co2_emissions_rule)
+
     def total_emissions_per_interval_rule(model, i):
-        return model.total_emissions_per_interval[i] == sum(model.co2_emissions[t] for t in range(i*intervals_time, (i+1)*intervals_time))
+        return model.total_emissions_per_interval[i] == sum(model.co2_emissions[h] for h in range(i*intervals_time, (i+1)*intervals_time))
     model.total_emissions_per_interval_constraint = Constraint(model.INTERVALS, rule=total_emissions_per_interval_rule)
 
-    def co2_emissions_rule(model, h):
-        return model.co2_emissions[h] == (
-            co2_data 
-        )
-    model.co2_emissions_constraint = Constraint(model.HOURS, rule=co2_emissions_rule)
+    def carbon_credits_needed_rule(model, i):
+        return model.carbon_credits[i] + model.credits_used_to_offset[i] >= model.total_emissions_per_interval[i] - max_co2_emissions
+    model.carbon_credits_needed_constraint = Constraint(model.INTERVALS, rule=carbon_credits_needed_rule)
+
+    def carbon_credits_limit_rule(model, i):
+        return model.carbon_credits[i] <= model.total_emissions_per_interval[i] - model.credits_earned[i]
+    model.carbon_credits_limit_constraint = Constraint(model.INTERVALS, rule=carbon_credits_limit_rule)
+
+    def carbon_credits_earned_rule(model, i):
+        if i == 0:
+            return model.credits_earned[i] == 0  # For the first interval
+        return model.credits_earned[i] == model.carbon_credits[i] - model.total_emissions_per_interval[i] + max_co2_emissions + model.credits_used_to_offset[i]
+    model.carbon_credits_earned_constraint = Constraint(model.INTERVALS, rule=carbon_credits_earned_rule)
+
+    # Ensure credits_unheld doesn't exceed credits earned and previously held
+    def credits_unheld_limit_rule(model, i):
+        if i == 0:
+            return model.credits_sold[i] <= model.credits_earned[i]  # For the first interval
+        return model.credits_sold[i] + model.credits_used_to_offset[i] <= model.credits_held[i - 1] + model.credits_earned[i]
+    model.credits_unheld_limit = Constraint(model.INTERVALS, rule=credits_unheld_limit_rule)
+
+    def credits_held_dynamics_rule(model, i):
+        if i == 0:
+            return model.credits_held[i] == model.credits_earned[i] - model.credits_used_to_offset[i]  # For the first interval
+        return model.credits_held[i] == model.credits_held[i - 1] + model.credits_earned[i] - model.credits_sold[i] - model.credits_used_to_offset[i]
+    model.credits_held_dynamics = Constraint(model.INTERVALS, rule=credits_held_dynamics_rule)
 
 
-    def capacity_rule(model, h):
-        return model.CCS_capacity >= (model.heat_production[h] + model.heat_stored[h]) + model.electricity_production[h]
-    model.capacity_constraint = Constraint(model.HOURS, rule=capacity_rule)
+
     # -------------- Objective Function --------------
     def ccs_objective_rule(model):
-        capital_cost = 3000 + 20 * model.CCS_capacity
-        cost = sum(model.ccs_investment_decision[t] * (ccs_cost_function + operating_costs) for t in model.TIME_PERIODS)
-        revenue = sum(model.carbon_captured[t] * carbon_market_price[t] for t in model.TIME_PERIODS)
-        return cost - revenue
+        capital_cost = (3000 + 20 * model.CCS_capacity) * sum(model.ccs_investment_decision[t] for t in model.TIME_PERIODS)
+        operating_cost = sum(model.ccs_investment_decision[t] * model.co2_emissions[t] * 1000 for t in model.TIME_PERIODS)
+        carbon_cost = sum((model.carbon_credits[i]) * carbon_market[i] for i in model.INTERVALS)
+        carbon_sold = sum(model.credits_sold[i] * carbon_market[i] for i in model.INTERVALS) * 0.01
+        return (operating_cost + capital_cost + carbon_cost) - carbon_sold 
     model.objective = Objective(rule=ccs_objective_rule, sense=minimize)
 
 
@@ -82,11 +188,18 @@ def pyomomodel():
         solver.options['NonConvex'] = 2
         solver.options['TimeLimit'] = 60
         solver.options["Threads"]= 16
-        solver.options["LPWarmStart "]= 2
+        solver.options["LPWarmStart "] = 2
     solver.solve(model, tee=True)
 
     return model
 
+def print_selected_CCS(model):
+    for t in model.TIME_PERIODS:
+        print(f"Month {t} - Bought? - {model.ccs_investment_decision[t].value}, {model.ccs_availability[t].value}")
+    for i in model.INTERVALS:
+        print(f"Interval {i} - Credits - {model.carbon_credits[i].value}, {model.credits_sold[i].value}")
 
 if __name__ == "__main__":
+    app.run_server(debug=True)
     model = pyomomodel()
+    print_selected_CCS(model)
